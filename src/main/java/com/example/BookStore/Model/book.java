09d3b015278a6a40e0d18e87f5ec837e77ba239d
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-27 12:40:19 +0300

    BookEntity

--- src/main/java/com/example/BookStore/Model/book.java
+++ src/main/java/com/example/BookStore/Model/book.java
@@ -1,6 +1,12 @@
 package com.example.BookStore.Model;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "book")
 public class book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
