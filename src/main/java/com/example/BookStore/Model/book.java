122aff66fde3b8956b9e7c22d4129bb555cc2033
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-28 11:29:12 +0300

    BookEntityDetails

--- src/main/java/com/example/BookStore/Model/book.java
+++ src/main/java/com/example/BookStore/Model/book.java
@@ -8,5 +8,63 @@ public class book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
+
+    private String name;
+
+    private String category;
+
+    private long datePublished;
+
+    // Empty constractor required by JPA
+    public book(){}
+
+    // Parameterized constractor
+    public book(String name, String category, long datePublished){
+        this.name = name;
+        this.category = category;
+        this.datePublished = datePublished;
+    }
+
+    /*
+     * Getters and Setters for books details.
+     * @param book
+     * @return book details
+     */
+
+    // Getter for id
+    public long getId(){
+        return this.id;
+    }
+
+    // Getter for name
+    public String getName(){
+        return this.name;
+    }
+
+    // Setter for name
+    public void setName(String name){
+        this.name = name;
+    }
+
+    // Getter for category
+    public String getCategory(){
+        return this.category;
+    }
+
+    // Setter for category
+    public void setCategory(String category){
+        this.category = category;
+    }
+
+    // Getter for published
+    public long getDatePublished(){
+        return this.datePublished;
+    }
+
+    // Setter for published
+    public void setDatePublished(long datePublished){
+        this.datePublished = datePublished;
+    }
+
 }
