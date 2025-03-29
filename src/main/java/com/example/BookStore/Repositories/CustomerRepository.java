375e7ed62ddd88f392b1e75483ace2ff92875d16
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-29 14:45:13 +0300

    CustomerRepository

--- src/main/java/com/example/BookStore/Repositories/CustomerRepository.java
+++ src/main/java/com/example/BookStore/Repositories/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.example.BookStore.Repositories;
 
+import com.example.BookStore.Model.CustomerModel;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CustomerRepository {
+public interface CustomerRepository  extends JpaRepository<CustomerModel, Long>{
 
 }
