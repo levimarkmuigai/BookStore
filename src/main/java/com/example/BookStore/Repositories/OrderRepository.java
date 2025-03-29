8dfa5e8ca23d38612c5bbca9422a0f35392a5d7e
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-29 21:07:04 +0300

    OrderRepository

--- src/main/java/com/example/BookStore/Repositories/OrderRepository.java
+++ src/main/java/com/example/BookStore/Repositories/OrderRepository.java
@@ -1,5 +1,10 @@
 package com.example.BookStore.Repositories;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.BookStore.Model.OrderModel;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderModel, Long>{
 
 }
