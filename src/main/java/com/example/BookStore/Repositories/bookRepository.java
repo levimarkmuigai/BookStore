7456467442b537a5a69523f09bd9c1637f57b016
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-29 13:33:39 +0300

    BookRepository

--- src/main/java/com/example/BookStore/Repositories/bookRepository.java
+++ src/main/java/com/example/BookStore/Repositories/bookRepository.java
@@ -1,6 +1,10 @@
 package com.example.BookStore.Repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository; 
+import com.example.BookStore.Model.bookModel;
+import org.springframework.stereotype.Repository;
 
-public interface bookRepository {
+@Repository
+public interface bookRepository extends JpaRepository<bookModel, Long>{
 
 }
