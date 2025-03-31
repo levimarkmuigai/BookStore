c32b4efa164e7890f79657a12458fe91e5f544dc
levimarkmuigai <levimarkmuigai@gmail.com>
2025-03-31 20:37:54 +0300

    BookControllerAddedToHandleWebRequests

--- src/main/java/com/example/BookStore/Controller/BookController.java
+++ src/main/java/com/example/BookStore/Controller/BookController.java
@@ -1,5 +1,16 @@
 package com.example.BookStore.Controller;
 
+import com.example.BookStore.Model.BookModel;
+import com.example.BookStore.Repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * REST controller for managing book entity.
+ */
+@RestController
+@RequestMapping("/api/book")
 public class BookController {
 
 }
