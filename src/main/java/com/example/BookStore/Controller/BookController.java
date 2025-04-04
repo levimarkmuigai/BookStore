128b233663afb11dbb745e60a932c6ff4286d446
levimarkmuigai <levimarkmuigai@gmail.com>
2025-04-04 21:57:51 +0300

    BookController

--- src/main/java/com/example/BookStore/Controller/BookController.java
+++ src/main/java/com/example/BookStore/Controller/BookController.java
@@ -14,36 +14,33 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/book")
-public class BookController {
+public class BookController{
 
-    private final BookRepository bookRepository;
+  private final BookRepository bookRepository;
 
-    @Autowired
-    public BookController(BookRepository bookRepository){
-        this.bookRepository = bookRepository;
-    }
+  @Autowired
+  public BookController(BookRepository bookRepository){
+    this.bookRepository = bookRepository;
+  }
 
-    /**
-     * Returns a list of all books
-     * 
-     * @return a list of book objects
-     */
-    @GetMapping
-    public List<BookModel> getAllBooks(){
-        return bookRepository.findAll();
-    }
+  /**
+    * Returns a list of all books
+    * 
+    * @return a list of book objects
+    */
+  @GetMapping
+  public List<BookModel> getAllBooks(){
+    return bookRepository.findAll();
+  }
 
-    /**
-     * Get book entity by the Id
-     * 
-     * @param id the book's id
-     * @return employee object wrapped in Response entity
-     */
-    @GetMapping({"/id"})
-    public ResponseEntity<BookModel> getBookById(@PathVariable Long id){
-        return bookRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-    }
-
-
-
+  /**
+    * Get book entity by the Id
+    * 
+    * @param id the book's id
+    * @return employee object wrapped in Response entity
+    */
+  @GetMapping({"/id"})
+  public ResponseEntity<BookModel> getBookById(@PathVariable Long id){
+    return bookRepository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+  }
 }
