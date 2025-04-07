8023d3c652a40923583be9fa3cc81d5d9e44763a
levimarkmuigai <levimarkmuigai@gmail.com>
2025-04-07 11:52:17 +0300

    AddedAMethodToUpdateExisitingBooks

--- src/main/java/com/example/BookStore/Controller/BookController.java
+++ src/main/java/com/example/BookStore/Controller/BookController.java
@@ -2,7 +2,6 @@ package com.example.BookStore.Controller;
 
 import com.example.BookStore.Model.BookModel;
 import com.example.BookStore.Repositories.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +16,6 @@ public class BookController {
 
   private final BookRepository bookRepository;
 
-  @Autowired
   public BookController(BookRepository bookRepository) {
     this.bookRepository = bookRepository;
   }
@@ -50,7 +48,27 @@ public class BookController {
    * @return the created book entity
    */
   @PostMapping
-  public BookModel createBook(@RequestBody BookModel bookModel) {
-    return bookRepository.save(bookModel);
+  public BookModel createBook(@RequestBody BookModel book) {
+    return bookRepository.save(book);
+  }
+
+  /**
+   * Updates exsiting book.
+   * 
+   * @param id         the book's id.
+   * @param updateBook updates the book's details
+   * @return the updated book's details.
+   */
+  @PutMapping("/{id}")
+  public ResponseEntity<BookModel> updateBook(@PathVariable Long id, @RequestBody BookModel updateBook) {
+    return bookRepository.findById(id).map(book -> {
+      book.setBookAuthor(updateBook.getBookAuthor());
+      book.setBookName(updateBook.getBookName());
+      book.setBookCategory(updateBook.getBookCategory());
+      book.setDatePublished(updateBook.getDatePublished());
+      BookModel savedBook = bookRepository.save(book);
+      return ResponseEntity.ok(savedBook);
+    })
+        .orElse(ResponseEntity.notFound().build());
   }
 }
