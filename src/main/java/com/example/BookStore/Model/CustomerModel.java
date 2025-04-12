352ee0fc1bd26e9cca75ed36ff8c3ab8563aee0a
levimarkmuigai <levimarkmuigai@gmail.com>
2025-04-12 13:15:18 +0300

    AddedJakartaConstraintsToAvoidsqlInjectionAddedFirstLastMiddleName

--- src/main/java/com/example/BookStore/Model/CustomerModel.java
+++ src/main/java/com/example/BookStore/Model/CustomerModel.java
@@ -1,6 +1,7 @@
 package com.example.BookStore.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 /**
  * Class CustomerModel for the Customer's details.
@@ -13,12 +14,42 @@ public class CustomerModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // For the customer's id
 
+    @Column(name = "FirstName", nullable = false)
+    @Size(max = 25, message = "Name should not exceed 25 characters.")
+    @NotNull(message = "FirstName should not be empty.")
+
+    private String firstName;
+
+    @Column(name = "MiddleName")
+    @Size(max = 25, message = "Name should not exceed 25 characters.")
+
+    private String middleName;
+
+    @Column(name = "LastName", nullable = false)
+    @Size(max = 25, message = "Name should not exceed 25 characters.")
+    @NotNull(message = "LastName should not be empty.")
+    private String lastName;
+
+    @Column(name = "UserName", nullable = false)
+    @Size(min = 5, max = 55, message = "UserName should be between 5 and 55 characters.")
+    @NotNull(message = "UserName cannot be empty.")
+
     private String customerUserName; // For the customer's username
+
+    @Column(name = "Email", nullable = false)
+    @Size(max = 255, message = "Email cannot be more than 255 characters.")
+    @NotNull(message = "Email cannot be empty.")
 
     private String customerEmail; // For the customer's email
 
+    @Column(name = "PhoneNumber", nullable = true)
+    @Positive(message = "A phonenumber should be positive (e.g 0712345678).")
+
     private long customerPhoneNumber; // For the customer's phonenumber
 
+    @Column(name = "Gender", nullable = false)
+    @Size(max = 7, message = "Gender should not be more than 7 characters.")
+    @NotNull(message = "Gender cannot be empty.")
     private String customerGender; // For the customer's gender
 
     /**
@@ -32,18 +63,28 @@ public class CustomerModel {
     /**
      * Parameterized class constructor.
      * 
+     * @param firstName
+     * @param middleName
+     * @param lastName
      * @param customerUserName
      * @param customerEmail
      * @param customerPhoneNumber
      * @param customerGender
      * 
+     * @return customer's first name.
+     * @return customer's middle name.
+     * @return customer's last name.
      * @return customer's username
      * @return customer's email
      * @return customer's phonenumber
      * @return customer's gender
      */
     public CustomerModel(String customerUserName, String customerEmail,
-            long customerPhoneNumber, String customerGender) {
+            long customerPhoneNumber, String customerGender, String firstName,
+            String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
         this.customerUserName = customerUserName;
         this.customerEmail = customerEmail;
         this.customerPhoneNumber = customerPhoneNumber;
@@ -54,6 +95,9 @@ public class CustomerModel {
      * Getters and Setters for Customer's details
      * 
      * @param id
+     * @param firstName
+     * @param middleName
+     * @param lastName
      * @param customerUserName
      * @param customerEmail
      * @param customerPhoneNumber
@@ -61,6 +105,7 @@ public class CustomerModel {
      * 
      * @return customer's details
      */
+
     /**
      * Getter for the customer's id.
      * 
@@ -68,6 +113,60 @@ public class CustomerModel {
      */
     public Long getId() {
         return this.id;
+    }
+
+    /**
+     * Getter for the Customer's first name
+     * 
+     * @return customer's first name
+     */
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * Setter for Customer's first name
+     * 
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Getter for the Customer's middle name
+     * 
+     * @return customer's middle name
+     */
+    public String getMiddleName() {
+        return this.middleName;
+    }
+
+    /**
+     * Setter for Customer's middle name
+     * 
+     * @param middleName
+     */
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    /**
+     * Getter for the Customer's last name
+     * 
+     * @return customer's last name
+     */
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * Setter for Customer's last name
+     * 
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     /**
