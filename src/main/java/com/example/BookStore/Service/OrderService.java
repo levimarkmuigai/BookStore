697aa35d94a17de693f63b6e309d0e2fd0af97d6
levimarkmuigai <levimarkmuigai@gmail.com>
2025-04-13 09:46:49 +0300

    ClassThatCreatesNewOrderSummaryInstance

--- src/main/java/com/example/BookStore/Service/OrderService.java
+++ src/main/java/com/example/BookStore/Service/OrderService.java
@@ -1,5 +1,29 @@
 package com.example.BookStore.Service;
 
-public class OrderService {
+import com.example.BookStore.DTO.OrderSummary;
+import com.example.BookStore.Repositories.OrderRepository;
+import com.example.BookStore.Model.OrderModel;
 
+import org.springframework.stereotype.Service;
+
+/**
+ * A class that creates a new instance of an
+ * order's summery.
+ */
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public OrderSummary generateOrderDetails(Long id) {
+        OrderModel order = orderRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Order not found."));
+
+        return new OrderSummary(
+                order.getId(),
+                order.getDate());
+    }
 }
