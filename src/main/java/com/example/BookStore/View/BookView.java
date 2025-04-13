efbdc56f33c7d84a548effd3cdcbf26bf830814f
levimarkmuigai <levimarkmuigai@gmail.com>
2025-04-13 09:29:43 +0300

    AClassThatTakesBookSummeryAndProducesDetailsInJsonFormat

--- src/main/java/com/example/BookStore/View/BookView.java
+++ src/main/java/com/example/BookStore/View/BookView.java
@@ -1,5 +1,35 @@
 package com.example.BookStore.View;
 
-public class BookView {
+import com.example.BookStore.Service.BookService;
+import com.example.BookStore.Repositories.BookRepository;
+import com.example.BookStore.DTO.BookSummery;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * A class that prints a book's details in Json Format.
+ */
+public class BookView {
+    public final BookRepository bookRepository;
+    public final BookService bookService;
+
+    public BookView(BookRepository bookRepository, BookService bookService) {
+        this.bookRepository = bookRepository;
+        this.bookService = bookService;
+    }
+
+    /**
+     * A method that prints a book's details
+     * in Json format.
+     * 
+     * @return a report of book's details.
+     */
+    @GetMapping("/api/bookreport")
+    public List<BookSummery> getBookReport(Long id) {
+        return bookRepository.findById(id).stream().map(book -> bookService.generateBookDetails(book.getId()))
+                .collect(Collectors.toList());
+    }
 }
